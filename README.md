b90371121dd364647cb78791e234ae6bd5a1425b
wirarya2005 <wirarya.tjoa@gmail.com>
2025-01-02 18:33:09 +0700

    final

---
+++ README.md
@@ -0,0 +1,29 @@
+# Discord-bot-AI-Groq-Llama
+
+## Jangan taruh token didalam file code, contohnya disini adalah `main.py`
+![cnth-token](/ss/n-bot.png)
+## - Pertama : Kamu bisa taruh token di dalam file `.env`
+![env](/ss/workspace.png)
+## - Kedua : Kamu bisa load token tersebut dengan cara : 
+![load](/ss/load_token.png)
+
+## Token Groq
+## - Pertama : Kamu bisa dapatkan token untuk AI kamu dari website [console.groq.com](https://console.groq.com/)
+## - Kedua : Kamu bisa masuk ke bagian `API Keys` untuk mendapatkan token
+![groq-dash](/ss/groq-dash.png)
+## - Ketiga : Klik button `Create API Key` untuk mendapatkan token 
+![groq-api](/ss/groq-key.png)
+
+## Token Discord Bot
+## - Pertama : Kamu bisa masuk ke website [discord developer portal](https://discord.com/developers/docs/intro)
+## - Kedua : Pada bagian `Applications`, kamu bisa klik button `New Application`:
+![dc-dash](/ss/dc-dash.png)
+## - Ketiga : Setelah masuk, pada bagian `Bot` kamu bisa klik `Reset Token` untuk mendapatkan token baru
+![restok](/ss/res-tok.png)
+
+## Model
+## Banyak pilihan model AI yang dapat digunakan. Bisa dilihat pada bagian `Documentation`, lalu `Models`
+![model](/ss/model.png)
+
+## Ketika di jalankan
+![pr](/ss/tes-bot.png)
---
+++ main.py
@@ -0,0 +1,68 @@
+# Import lib yang dibutuhkan
+from groq import Groq
+from dotenv import load_dotenv
+import os
+from typing import Final
+import discord
+from discord.ext import commands
+
+# Load Macam Token
+load_dotenv()
+TOKEN_BOT: Final[str] = os.getenv('BOT_TOKEN')
+TOKEN_GROQ: Final[str] = os.getenv('AI_KEY')
+
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Inisialisasi bot
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+ai_groq = Groq(api_key=TOKEN_GROQ)
+
+# Event dan command buat bot Tams
+@bot.event
+async def on_ready():
+    print(f'{bot.user} Siap digunakan')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello! Saya Tams, Asistem Pribadi Kamu!, Ada yang bisa saya bantu?')
+
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send("Goodbye! Sampai Jumpa Lagi!")
+
+# Groq Respon AI
+@bot.command()
+async def tanya(ctx, *, pertanyaan):
+    try:
+        chat_ai = ai_groq.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"""Jawab pertanyaan ini dalam bahasa indonesia : {pertanyaan}"""
+                }
+            ], 
+            model="llama-3.3-70b-versatile",
+            stream=False
+        )
+        ai_output = chat_ai.choices[0].message.content
+        await ctx.send(ai_output)
+    except:
+        await ctx.send("Maaf, saya tidak mengerti")
+
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1324013904384229460)  
+    if channel:
+        await channel.send(f"Hello {member.name}, Selamat Datang Di Server!")
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1324013904384229460)  
+    if channel:
+        await channel.send(f"{member.name} Telah meninggalkan Server!")
+
+bot.run(TOKEN_BOT)
